e1ba27ba8cf3c43c40719f2b1d46b3e0cde42a08
Abdullatif Mnyamisi <abdullatifmnyamis@gmail.com>
2025-08-13 07:14:46 -0700

    resolved error: by changing anobject {API_URL} to API_URL

--- src/components/Dashboard/FamilyTab.jsx
+++ src/components/Dashboard/FamilyTab.jsx
@@ -32,7 +32,7 @@ const FamilyTab = () => {
 
   const API_URL = import.meta.env.VITE_API_URL;
   const api = axios.create({
-    baseURL: {API_URL},
+    baseURL: API_URL,
     headers: {
       Authorization: `Bearer ${token}`,
     },
